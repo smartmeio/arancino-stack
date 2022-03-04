22a3592420189badefb61ddcbe5b993029344279
Arturo Rinaldi <arty.net2@gmail.com>
2022-03-04 16:31:14 +0100

    export locale to C.UTF-8 in nRF52 upload script

--- files/run-arancino-adafruit-nrfutil.sh
+++ files/run-arancino-adafruit-nrfutil.sh
@@ -14,6 +14,10 @@ elif [[ "${2##*.}" = "zip" ]]; then
   mv $2 /tmp/sketch.zip
 fi
 
+# LOCALES
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
 # echo out > /sys/class/gpio/gpio25/direction
 # echo 1 > /sys/class/gpio/gpio25/value
 
@@ -25,13 +29,13 @@ sleep 3
 
 port=${1}
 
-# bossac actual upload
+# nRF52 actual upload
 echo "upload port is $1"
 adafruit-nrfutil --verbose dfu serial -pkg "/tmp/sketch.zip" -p ${port} -b 115200 --singlebank
 
 rc=$?
 
-rm /tmp/sketch.bin
+rm /tmp/sketch.zip
 
 # echo in > /sys/class/gpio/gpio25/direction
 
